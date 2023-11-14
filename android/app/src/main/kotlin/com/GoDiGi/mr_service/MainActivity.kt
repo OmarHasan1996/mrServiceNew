a1652cc8f3d0d81225f86c606f33031cbd1c097e
omar Hasan <omar.suhail.hasan@gmail.com>
2023-11-14 05:19:35 +0200

    icon, splash

---
+++ android/app/src/main/kotlin/com/GoDiGi/mr_service/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.GoDiGi.mr_service
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/godigi/closer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.godigi.closer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
